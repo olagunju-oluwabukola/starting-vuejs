9babd794e534b5a660f85183f0156483a4abd1a5
olagunju-oluwabukola <olagunjuoluwabukola2018@gmail.com>
2024-02-06 22:16:22 +0100

    vue directives

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import NewComponent from './components/NewComponent.vue'
+import Component2 from './Component2.vue'
+</script>
+
+<template>
+  <NewComponent />
+<Component2/>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/NewComponent.vue
@@ -0,0 +1,61 @@
+<template>
+    <h1>{{ title }} - {{ author }}</h1>
+    <input type="text" ref="name">
+    <button @click="handleClick" >Click Me</button>
+    <button v-bind:open="isDisable">Bind</button>
+    <h2 class="underline">Underline</h2>
+<h2 v-bind:class="status">Status </h2>
+<h2 v-bind:class="isPromoted && 'promoted'">Promoted movie</h2>
+<h2 v-bind:class="isSoldOut ? 'soldout' : 'new'" >Sold out? movie</h2>
+<h2 v-bind:style="dangerStyle">Danger style</h2>
+<h2 v-bind:style="background">Success Style</h2>
+<h2  class="gold" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, modi?</h2>
+<div>
+    <h2 v-if="num===0">Number is Zero</h2>
+    <h2 v-else-if="num > 0"> It is a positive number😘</h2>
+    <h2 v-else-if="num < 0">It is a negative number😡</h2>
+    <h2 v-else> Number is a negative</h2>
+    
+    
+</div>
+<h2 v-show="hideElement">Using v-show</h2>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            num: 0,
+            hideElement:true,
+            name: 'app',
+            title: 'My First Vue App',
+            author : 'Me',
+            isDisable: false,
+            isPromoted: true,
+            status: 'success',
+            isSoldOut: true,
+            style: 'gold',
+             dangerStyle:{
+                color: 'red',
+                'font-size': '2em'
+            
+             },
+             background: 'color:green',
+             font :50,
+            
+
+
+        }
+    },
+    methods: {
+        handleClick(){
+            console.log('clicked')
+        }
+    }
+}
+
+</script>
+
+<style scoped>
+
+</style>
